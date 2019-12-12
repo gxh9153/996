e26f253ec9ac673b1db6cd40d615b14aee1e9887
gongxihui <1320546563@qq.com>
2019-12-12 10:38:21 +0800

    初始化;

---
+++ build/precache-manifest.0e5d8def3804d6fd7b7d6bb7255753a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b501dbbf8b8ec53102e1c86bdef0bf4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8094cf6a86c82fb188c",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0222a2868ccd3210e952",
+    "url": "./static/js/2.98a0de8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.98a0de8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8094cf6a86c82fb188c",
+    "url": "./static/js/main.80394f67.chunk.js"
+  },
+  {
+    "revision": "6c592fe6ca4c47fd7c62",
+    "url": "./static/js/runtime-main.05e9f3a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
